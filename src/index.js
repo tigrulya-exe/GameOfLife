43833e0afe414280d7755eb159114d34061f0c0c
Tigran Manasyan <manasyan_tigran@mail.ru>
2020-06-11 23:06:02 +0700

    react-create-app on Heroku

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
